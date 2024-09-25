11cfd191dec9b30c1c8e488f1ddcf013a2441077
vladislavvilchynskyi <vilchynskyi.v28@gmail.com>
2024-09-25 20:59:32 +0300

    Adding task#3.1 solution

---
+++ homework_for_task#3.1.py
@@ -0,0 +1,24 @@
+num_1 = float(input("Type your first number: "))
+num_2 = float(input("Type your second number: "))
+operation = input("Choice and type the operation what you want to do  / , *, + or - : ")
+
+if num_2 == 0 and operation == "/":
+    print("Oh Bro I`m sorry, but cannot be divided by zero!", "Please, type another number, thanks:)", sep= '\n')
+    num_2 = float(input("Type your second number: "))
+
+#   float обрав тому що таким чином калькулятор не буде видавати помилку,
+#   в разі як юзер захоче провести операції над нецілими числами,
+#   якщо буде перетворення в int - буде обрізаний калькулятор
+
+if operation == "/":
+    result = num_1 / num_2
+    print(result)
+elif operation == "*":
+    result = num_1 * num_2
+    print(result)
+elif operation == "+":
+    result = num_1 + num_2
+    print(result)
+elif operation == "-":
+    result = num_1 - num_2
+    print(result)
