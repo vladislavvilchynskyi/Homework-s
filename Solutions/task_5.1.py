332a6e04955cca23673b5bf7e598463cb537ac71
vladvilchunskyi <vilchynskyi.v28@gmail.com>
2024-10-06 13:20:34 +0300

    adding task_5.2

---
+++ Solutions/task_5.1.py
@@ -0,0 +1,22 @@
+import keyword
+import string
+
+num_all = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+user_string = input("Enter your value: ")
+if user_string not in keyword.kwlist:
+    if user_string not in string.punctuation:
+        if user_string.startswith(num_all):
+            print(False)
+        else:
+            if user_string.islower():
+                print(True)
+            else:
+                print(False)
+    elif user_string == "_":
+        print(True)
+    else:
+        print(False)
+else:
+    print(False)
+
+
