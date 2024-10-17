f112bac5b4514ed8c5a287ab2984fead23944c5d
vladvilchunskyi <vilchynskyi.v28@gmail.com>
2024-10-17 19:34:25 +0300

    adding minor update

--- Solutions/task_5.1.py
+++ Solutions/task_5.1.py
@@ -3,20 +3,28 @@ import string
 
 num_all = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
 user_string = input("Enter your value: ")
-if user_string not in keyword.kwlist:
-    if user_string not in string.punctuation:
-        if user_string.startswith(num_all):
-            print(False)
-        else:
-            if user_string.islower():
-                print(True)
-            else:
-                print(False)
-    elif user_string == "_":
-        print(True)
-    else:
+# if user_string not in keyword.kwlist:
+#     if user_string not in string.punctuation:
+#         if user_string.startswith(num_all):
+#             print(False)
+#         else:
+#             if user_string.islower():
+#                 print(True)
+#             else:
+#                 print(False)
+#     elif user_string == "_":
+#         print(True)
+#     else:
+#         print(False)
+# else:
+#     print(False)
+for i in user_string:
+    list_1 = keyword.kwlist
+    if i == user_string.isdigit():
         print(False)
-else:
-    print(False)
+    elif i == string.punctuation:
+        print(False)
+    elif i
+
 
 
--- Solutions/task_8.3.py
+++ Solutions/task_8.3.py
@@ -1,15 +1,18 @@
 def find_unique_value(some_list):
 
-    count_dict = {}
-    for item in some_list:
-        if item in count_dict:
-            count_dict[item] += 1
-        else:
-            count_dict[item] = 1
-
-    for item, count in count_dict.items():
-        if count == 1:
-            return item
+    # count_dict = {}
+    # for item in some_list:
+    #     if item in count_dict:
+    #         count_dict[item] += 1
+    #     else:
+    #         count_dict[item] = 1
+    #
+    # for item, count in count_dict.items():
+    #     if count == 1:
+    #         return item
+    for value in set(some_list):
+        if some_list.count(value) == 1:
+            return  value
 
 assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
 assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
